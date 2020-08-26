1cc1cbffd2b4257160427a87b01886b697ec0d78
Isuri97 <isurim@embla.asia>
2020-08-26 18:30:57 +0530

    add hello_name.cpp

---
+++ favorite_food.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string food;
+    cout << "Please enter your favorite food name: " << endl;
+    cin >> food;
+    cout << "My favorite food is " << food << endl;
+}
