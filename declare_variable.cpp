67b2b70ba006ff9393fb077263c4639e1aef8bf3
Isuri97 <isurim@embla.asia>
2020-08-26 21:31:44 +0530

    add variable declaration

---
+++ declare_variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 5;
+    int y = 10;
+
+    cout << x+y << endl;
+    cout << "Sum is " << x+y ;
+}
---
+++ sum_of_user_given_number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    int sum=0;
+
+    cout << "Enter first number: " << endl;
+    cin >> x;
+
+    cout << "Enter second number: " << endl;
+    cin >> y;
+
+    sum = x+y;
+
+    cout << "Answer is " << x+y << endl;
+    cout << "Sum is " << sum << endl ;
+}
